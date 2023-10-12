ed62bd938d96f2a3aefeca723ae90f0bf0b76d5d
Matisse Pedron <matisse@ecobosto.fr>
2023-10-12 18:58:32 +0200

    better than previous commit

--- index.js
+++ index.js
@@ -2,72 +2,80 @@ function clean(s) {
     return s.replace(/(<([^>]+)>)/gi, "").replace(/ /g, "").replace(/\"/g, "").replace(/&nbsp;/g, "").replace(/\n/g, "").replace(/,/g, "").trim().toUpperCase().replace(/S/g, "");
 }
 
+function cleanQuestion(q) {
+    return clean(q).replace(/[0-9]RÉPONECORRECTE$/g, "").replace(/[0-9]RÉPONE$/g, "").replace(/PLUIEURRÉPONE$/g, "");
+}
+
 function extractProp(prop) {
-    return prop.children[2]
-        ? clean(prop.children[2].children[1].children[0].innerHTML)
-        : clean(prop.children[1].children[1].children[0].innerHTML);
+    if (prop.children[2]) {
+        return clean(prop.children[2].children[1].children[0].innerHTML);
+    }
+    else if (prop.children[1].children.length != 0) {
+        return clean(prop.children[1].children[1].children[0].innerHTML);
+    }
+    else {
+        return clean(prop.children[1].innerHTML);
+    }
 }
 
 function extractSelectorProp(prop) {
     return clean(prop.children[0].children[0].innerHTML);
 }
 
-document.addEventListener('dblclick', () => {
+let file = null;
+fetch("https://raw.githubusercontent.com/matissePe/sdt/moodle-ubs/is.json")
+    .then(f => f.json())
+    .then(f => file = f);
 
+document.addEventListener('dblclick', () => {
     let question = document.getElementsByClassName("qtext")
 
     if (question.length > 0) {
-        question = clean(question[0].innerHTML)
+        question = cleanQuestion(question[0].innerHTML);
+        responses = file[question];
 
-        fetch(chrome.runtime.getURL('is.json'))
-            .then(file => file.json())
-            .then(file => {
+        if (responses) {
+            selector = false;
 
-                responses = file[question];
+            if (typeof responses === 'object' && responses.length === undefined) {
+                responses = Object.keys(responses);
+                selector = true;
+            }
 
-                if (responses) {
-                    selector = false;
+            responses.forEach(response => {
+                answerProp = selector
+                    ? document.getElementsByClassName("answer")[0].children[0].children
+                    : document.getElementsByClassName("answer")[0].children;
 
-                    if (typeof responses === 'object' && responses.length === undefined) {
-                        responses = Object.keys(responses);
-                        selector = true;
-                    }
+                for (let i = 0; i < answerProp.length; i++) {
 
-                    responses.forEach(response => {
-                        answerProp = selector
-                            ? document.getElementsByClassName("answer")[0].children[0].children
-                            : document.getElementsByClassName("answer")[0].children;
+                    // if selector menu
+                    if (selector) {
+                        let prop = extractSelectorProp(answerProp[i]);
 
-                        for (let i = 0; i < answerProp.length; i++) {
+                        if (prop == response) {
+                            responseSelector = file[question][response];
+                            selectorPropositions = answerProp[i].children[1].children[1].children;
 
-                            // if selector menu
-                            if (selector) {
-                                let prop = extractSelectorProp(answerProp[i]);
-
-                                if (prop == response) {
-                                    responseSelector = file[question][response];
-                                    selectorPropositions = answerProp[i].children[1].children[1].children;
-
-                                    for (let j = 0; j < selectorPropositions.length; j++) {
-                                        if (clean(selectorPropositions[j].innerHTML) == responseSelector) {
-                                            selectorPropositions[j].selected = true;
-                                        }
-                                    }
-                                }
-                            }
-
-                            // if radio button or checkbox
-                            else {
-                                let prop = extractProp(answerProp[i]);
-
-                                if (prop == response) {
-                                    answerProp[i].children[1].checked = true;
-                                    answerProp[i].children[0].checked = true;
+                            for (let j = 0; j < selectorPropositions.length; j++) {
+                                if (clean(selectorPropositions[j].innerHTML) == responseSelector) {
+                                    selectorPropositions[j].selected = true;
                                 }
                             }
                         }
-                    });
+                    }
+
+                    // if radio button or checkbox
+                    else {
+                        let prop = extractProp(answerProp[i]);
+
+                        if (prop == response) {
+                            answerProp[i].children[1].checked = true;
+                            answerProp[i].children[0].checked = true;
+                        }
+                    }
                 }
-            })
+            });
+        }
     }
 });
